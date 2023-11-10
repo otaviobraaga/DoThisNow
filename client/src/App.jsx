22093364ec14af617b2ba9d8c1adcc1d36ef4449
otavioobraga <otavioobraga@gmail.com>
2023-11-10 13:48:50 -0400

    Adicionado novas funcionalidades ao forma e submit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Todo from "./component/Todo";
 import TodoForm from "./component/TodoForm";
 
 function App() {
-  const [todos, todosAll] = useState([
+  const [todos, setTodos] = useState([
     {
       id: 1,
       text: "Criar funcionalidade no sistema",
@@ -24,15 +24,30 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo}/>
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
-        <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
